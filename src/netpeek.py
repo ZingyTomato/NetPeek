9a0bee077ec226712141ac68906a221f322413b7
ZingyTomato <79736973+ZingyTomato@users.noreply.github.com>
2025-08-07 21:40:00 +0530

    Fix issue with metainfo.xml

--- src/netpeek.py
+++ src/netpeek.py
@@ -27,7 +27,7 @@ gi.require_version('Adw', '1')
 import sys
 from .app import NetworkScannerApp
 
-def main(version="0.2.3"):
+def main(version="0.2.3.1"):
     """Main entry point for the application"""
     app = NetworkScannerApp()
     app.version = version
